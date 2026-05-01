5a584831cc710eaa370765f2e6e40efbd355b6d7
bbkingisking <email@example.com>
2026-05-01 23:02:46 +0100

    Build path follows env var

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: process.env.STATIC_DIR || 'dist',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
