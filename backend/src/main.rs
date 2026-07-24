855e02b27a2855e780f195c49a088d5919a87748
bbkingisking <info@example.com>
2026-07-24 15:14:23 +0100

    Fixed tracing subscriber crate name

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -37,7 +37,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "annyeong_backend=debug,tower_http=debug,axum=debug".into()),
+                .unwrap_or_else(|_| "hwaiting=debug,tower_http=debug,axum=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
