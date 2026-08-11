620c4a53c3f411570113bfa708706be858c049cc
bbkingisking <info@example.com>
2026-08-11 02:33:22 +0100

    Changed default logging level

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -49,7 +49,7 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "hwaiting=debug,tower_http=debug,axum=debug".into()),
+                .unwrap_or_else(|_| "hwaiting=info,tower_http=info,axum=info".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
